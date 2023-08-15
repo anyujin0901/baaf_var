5b859bddf3b1422f9c8160a5b9b20071d2f795ab
Ricardo <95429842+anyujin0901@users.noreply.github.com>
2023-08-15 10:58:41 +0100

    Update freezer.py

--- freezer.py
+++ freezer.py
@@ -1,30 +1,36 @@
 def freeze_decoder_weights(model, eachbatch):
+    # Train both the encoder and Rmodel1 for the first 40k batches
+    if eachbatch <= 40000:
+        for param in model.Tmodel.parameters():
+            param.requires_grad = True
+        for param in model.Rmodel1.parameters():
+            param.requires_grad = True
+        for param in model.Rmodel2.parameters():
+            param.requires_grad = False
+        for param in model.Rmodel3.parameters():
+            param.requires_grad = False
 
-    # Decide which decoder to freeze based on eachbatch
-    if (eachbatch-1) % 100 == 0:
-        x = ((eachbatch - 1) / 100) % 3
-        if x == 0:
-            for param in model.Rmodel1.parameters():
-                param.requires_grad = True
-            for param in model.Rmodel2.parameters():
-                param.requires_grad = False
-            for param in model.Rmodel3.parameters():
-                param.requires_grad = False
-        elif x == 1:
-            for param in model.Rmodel1.parameters():
-                param.requires_grad = False
-            for param in model.Rmodel2.parameters():
-                param.requires_grad = True
-            for param in model.Rmodel3.parameters():
-                param.requires_grad = False
-        elif x == 2:
-            for param in model.Rmodel1.parameters():
-                param.requires_grad = False
-            for param in model.Rmodel2.parameters():
-                param.requires_grad = False
-            for param in model.Rmodel3.parameters():
-                param.requires_grad = True
+    # Train Rmodel2 for the next 40k batches
+    elif 40000 < eachbatch <= 80000:
+        for param in model.Tmodel.parameters():
+            param.requires_grad = False
+        for param in model.Rmodel1.parameters():
+            param.requires_grad = False
+        for param in model.Rmodel2.parameters():
+            param.requires_grad = True
+        for param in model.Rmodel3.parameters():
+            param.requires_grad = False
 
+    # Train Rmodel3 for batches beyond 40k
+    else:
+        for param in model.Tmodel.parameters():
+            param.requires_grad = False
+        for param in model.Rmodel1.parameters():
+            param.requires_grad = False
+        for param in model.Rmodel2.parameters():
+            param.requires_grad = False
+        for param in model.Rmodel3.parameters():
+            param.requires_grad = True
 
 def check_requires_grad(model):
     """Check and print the parameters that are frozen or not."""
